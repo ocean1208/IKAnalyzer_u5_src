93d021a2efe5dd852be4dca9af78be87d5ca94d1
ocean1208 <baiming1208@gmail.com>
2013-11-01 17:59:45 +0800

    first commit.

---
+++ IKAnalyzer_u5_src/src/com/yiming/data/DataMiningLearning.java
@@ -0,0 +1,13 @@
+package com.yiming.data;
+
+public class DataMiningLearning {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello big data.CDO is the chief data officer.");
+	}
+
+}
